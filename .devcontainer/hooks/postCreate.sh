de3b91e7190c001ba50d634e1db89061c604fc12
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-06-25 08:09:27 +0000

    more stuff

---
+++ .devcontainer/hooks/postCreate.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# postCreate.sh is called after the container is created by devcontainer.json
+# It can be used to install & setup tools not wanted in the Dockerfile
---
+++ .devcontainer/hooks/postStart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# postStart.sh is called every time the container starts by devcontainer.json
+# It can be for misc tasks like ensuring all dependencies are installed
+
+# Due to https://github.com/microsoft/vscode-remote-release/issues/6683
+# we have to add workspace folder as trusted by git
+# Due to https://github.com/microsoft/vscode-remote-release/issues/6810#issuecomment-1159354677
+# this cannot be done in Dockerfile (else VSCode fails to configure git in the container)
+git config --global --add safe.directory /code
