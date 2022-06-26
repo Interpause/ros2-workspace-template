dc805216c4b717f67e7a8c7a7726258211753569
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-06-26 18:13:08 +0800

    better place for auto activating

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -7,6 +7,3 @@
 . /opt/ros/$ROS_DISTRO/setup.sh
 sudo rosdep install -i --from-path /code/src --rosdistro $ROS_DISTRO -y
 sudo colcon build --symlink-install
-
-# Auto-activate ros environment for any shell
-echo "source /opt/ros/$ROS_DISTRO/setup.bash\nsource /code/install/local_setup.bash" | sudo tee -a ~/.bashrc > /dev/null
